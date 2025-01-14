ecebe223085a1b2cb54f50d49781b35f73305d43
Dorian <dorian.dziula@gmail.com>
2025-01-14 18:07:55 +0100

    dodanie strony html projektu

---
+++ html/js/skrypty.js
@@ -0,0 +1,15 @@
+function showDivDownload() {
+   document.getElementById('instruction_download_id').style.display = "block";
+}
+
+function hideDivDownload() {
+   document.getElementById('instruction_download_id').style.display = "none";
+}
+
+function showDivProjectInfo() {
+   document.getElementById('instruction_project_id').style.display = "block";
+}
+
+function hideDivProjectInfo() {
+   document.getElementById('instruction_project_id').style.display = "none";
+}
