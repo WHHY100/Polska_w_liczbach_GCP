c10dfa830d2c28976bb6b1e3cc1b3bb047484d63
Dorian <dorian.dziula@gmail.com>
2025-01-28 12:58:56 +0100

    dodanie informacji o adresach w Polsce

---
+++ szkielet_bazy/source/tab_teryt_miejscowosci.sql
@@ -0,0 +1,13 @@
+create table tab_teryt_miejscowosci
+(
+    id                INT64,
+    data_generacji    TIMESTAMP,
+    wojewodztwo_nazwa STRING,
+    powiat_nazwa      STRING,
+    gmina_nazwa       STRING,
+    gmina_typ         STRING,
+    miejscowosc_nazwa STRING,
+    ulica_cecha       STRING,
+    ulica_nazwa       STRING
+);
+
